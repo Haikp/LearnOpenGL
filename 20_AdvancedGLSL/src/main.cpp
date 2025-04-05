ce91973659f95c4711f4052ebabc42987f1201f0
Koi <kelvindo2002@gmail.com>
2025-04-04 23:43:38 -0700

    almost done with glsl data management module

---
+++ 20_AdvancedGLSL/src/main.cpp
@@ -0,0 +1,240 @@
+#include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <cmath>
+#include <vector>
+#include "shaders.h"
+#include "camera.h"
+//#include "bettermesh.h"
+//#include "model.h"
+#include "officialmesh.h"
+#include "officialmodel.h"
+
+#define STRINGIFY(x) #x
+#define TOSTRING(x) STRINGIFY(x)
+
+#include <stb_image.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <assimp/Importer.hpp>
+#include <assimp/scene.h>
+#include <assimp/postprocess.h>
+
+void processInput(GLFWwindow*);
+void scroll_callback(GLFWwindow*, double, double);
+glm::mat4 myLookAt(glm::vec3, glm::vec3, glm::vec3);
+
+//just for dynamic purposes for future projects
+std::string path_to_vertex_shader = std::string(TOSTRING(SHADER_PATH)) + "vertex_shader.glsl";
+std::string path_to_fragment_shader = std::string(TOSTRING(SHADER_PATH)) + "fragment_shader.glsl";
+std::string path_to_points_vertex_shader = std::string(TOSTRING(SHADER_PATH)) + "points_vertex_shader.glsl";
+std::string path_to_points_fragment_shader = std::string(TOSTRING(SHADER_PATH)) + "points_fragment_shader.glsl";
+std::string path_to_textures = std::string(TOSTRING(TEXTURE_PATH));
+std::string path_to_models = std::string(TOSTRING(MODEL_PATH));
+
+const int xWindowSize = 1080;
+const int yWindowSize = 720;
+
+float fov = 45.0f;
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(xWindowSize, yWindowSize, "LearnOpenGL", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    GLenum err = glewInit();
+    if (err != GLEW_OK)
+    {
+        std::cerr << "Failed to init glew" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    float cubeVertices[] = {
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+        0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+        0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+    };
+
+    float points[] = {
+        -0.7f,  0.5f,  0.0f,  // Point 1
+        0.2f, -0.3f,  0.0f,  // Point 2
+        -0.5f, -0.8f,  0.0f,  // Point 3
+        0.8f,  0.6f,  0.0f,  // Point 4
+        0.0f,  0.0f,  0.0f,  // Point 5 (center)
+        -0.9f,  0.2f,  0.0f,  // Point 6
+        0.5f, -0.7f,  0.0f,  // Point 7
+        0.4f,  0.8f,  0.0f,  // Point 8
+    };
+
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+
+    glBindVertexArray(0);
+
+    unsigned int pointVAO, pointVBO;
+    glGenVertexArrays(1, &pointVAO);
+    glBindVertexArray(pointVAO);
+
+    glGenBuffers(1, &pointVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, pointVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), &points, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+
+    glBindVertexArray(0);
+
+    //compile shaders
+    Shader shaderProgram(path_to_vertex_shader.c_str(), path_to_fragment_shader.c_str());
+    Shader pointsShader(path_to_points_vertex_shader.c_str(), path_to_points_fragment_shader.c_str());
+
+    //ubos
+    unsigned int ubo;
+    glGenBuffers(GL_UNFORM_BUFFER, &ubo);
+    glBindBuffer(GL_UNFORM_BUFFER, ubo);
+    glBufferData(GL_UNFORM_BUFFER, 32, NULL, GL_STATIC_DRAW);
+    glBindBuffer(GL_UNFORM_BUFFER, 0);
+
+    glm::vec3 cameraPosition = glm::vec3(0.0f, 0.0f, 10.0f);
+    glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+    glm::vec3 cameraOrientiation = glm::vec3(0.0f, 1.0f, 0.0f);
+
+    Camera camera(cameraPosition, cameraFront, cameraOrientiation, xWindowSize, yWindowSize);
+
+    glm::mat4 projection = glm::perspective(glm::radians(fov), ((float)xWindowSize / (float)yWindowSize), 0.1f, 100.0f);
+
+    glEnable(GL_PROGRAM_POINT_SIZE);
+
+    glEnable(GL_DEPTH_TEST);
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(.1f, .1f, .1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        processInput(window);
+        camera.TakeInputs(window);
+
+        shaderProgram.use();
+        shaderProgram.setVec3("cameraPos", camera.getPosition());
+        shaderProgram.setMat4("projection", projection);
+        shaderProgram.setMat4("view", camera.getViewMat());
+
+        glm::mat4 model = glm::mat4(1.0f);
+        shaderProgram.setMat4("model", model);
+
+        //draw cubes
+        glBindVertexArray(VAO);
+        glActiveTexture(GL_TEXTURE0);
+
+        model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(-1.0f, 0.0f, -1.0f)); // translate it down so it's at the center of the scene
+        shaderProgram.setMat4("model", model);
+
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        pointsShader.use();
+        glBindVertexArray(pointVAO);
+        pointsShader.setMat4("projection", projection);
+        pointsShader.setMat4("view", camera.getViewMat());
+        pointsShader.setMat4("model", model);
+
+        glDrawArrays(GL_POINTS, 0, 8);
+
+        glfwSetScrollCallback(window, scroll_callback);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glDeleteVertexArrays(1, &VAO);
+
+    glDeleteBuffers(1, &VBO);
+
+    glfwTerminate();
+    return 0;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS && glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+    fov -= (float)yoffset;
+    if (fov < 1.0f)
+        fov = 1.0f;
+    if (fov > 45.0f)
+        fov = 45.0f;
+}
